fb79c4f68998048b5774f4a3913faa2d77e11fbe
Paweł Szczur <pawelszczur@gmail.com>
2018-02-10 00:52:53 +0100

    Add Http trivial handler.

--- cmd/diq/diq.go
+++ cmd/diq/diq.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true})
+	logrus.SetOutput(os.Stderr)
+
+	logrus.Info("Hello world!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logrus.Infof("got a request!")
+		fmt.Fprintf(w, "Hello world!")
+	})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		logrus.Errorf("Problem listening: %s", err)
+	}
 }
