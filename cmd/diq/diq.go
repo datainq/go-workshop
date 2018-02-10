6298d7d574597eefb18d0080d6fbae97ae859eed
Paweł Szczur <pawelszczur@gmail.com>
2018-02-10 01:45:19 +0100

    Serve HTML from file.

--- cmd/diq/diq.go
+++ cmd/diq/diq.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"path"
 
 	"github.com/sirupsen/logrus"
+)
+
+var (
+	tmplDir   = "./tmpl"
+	staticDir = "./static"
 )
 
 func main() {
@@ -15,9 +21,18 @@ func main() {
 	logrus.Info("Hello world!")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		logrus.Infof("got a request!")
-		fmt.Fprintf(w, "Hello world!")
+		logrus.Info("got a request!")
+		f, err := os.Open(path.Join(tmplDir, "index.html"))
+		if err != nil {
+			logrus.Errorf("cannot open file: %s", err)
+		}
+		defer f.Close()
+		if _, err = io.Copy(w, f); err != nil {
+			logrus.Errorf("problem copying HTML: %s", err)
+		}
 	})
+	http.Handle("/static/",
+		http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		logrus.Errorf("Problem listening: %s", err)
 	}
